a9c1c056827229fc6d98990eb4824865b8194065
Aj Castiglione <antoniocastiglione10@gmail.com>
2020-02-23 10:24:37 -0500

    updates from Feb

--- partials/content-about.php
+++ partials/content-about.php
@@ -2,29 +2,38 @@
 
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	  <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+      <article id="post-<?php the_ID(); ?>" <?php post_class('cf'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-		  <header class="article-header">
-			  <h1 class="page-title">
-          <?php $title = get_field('page_title'); if(!empty($title)) : echo $title; else : the_title(); endif; ?>
-        </h1>
-			</header>
+        <header class="article-header">
+          <h1 class="page-title">
+            <?php $title = get_field('page_title');
+            if (!empty($title)) : echo $title;
+            else : the_title();
+            endif; ?>
+          </h1>
+        </header>
 
-			<section class="entry-content cf large-wrapper" itemprop="articleBody">
-        <div class="flex">
-          <div class="left">
-            <?php if(have_rows('content_images')) : while(have_rows('content_images')) : the_row(); ?>
-              <?php $img = get_sub_field('image'); if(!empty($img)) {echo "<img src='$img[url]' alt='$img[alt]' class='image'/>";} ?>
-            <?php endwhile; endif; ?>
+        <section class="entry-content cf large-wrapper" itemprop="articleBody">
+          <div class="flex">
+            <div class="left">
+              <?php if (have_rows('content_images')) : while (have_rows('content_images')) : the_row(); ?>
+                  <?php $img = get_sub_field('image');
+                  if (!empty($img)) {
+                    echo "<img src='$img[url]' alt='$img[alt]' class='image'/>";
+                    if ($img['caption']) echo "<caption>$img[caption]</caption>";
+                  } ?>
+              <?php endwhile;
+              endif; ?>
+            </div>
+            <div class="right">
+              <?php the_content(); ?>
+            </div>
           </div>
-          <div class="right">
-            <?php the_content(); ?>
-          </div>
-        </div>
-			</section>
+        </section>
 
-		</article>
+      </article>
 
-	<?php endwhile; endif; ?>
+  <?php endwhile;
+  endif; ?>
 
 </main>
